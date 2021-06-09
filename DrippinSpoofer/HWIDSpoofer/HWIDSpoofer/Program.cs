db40f5409d141caaacccaaa3d7966482795ec10f
Detroitt <83370815+Detroitt@users.noreply.github.com>
2021-06-09 13:58:29 -0300

    Add files via upload

---
+++ DrippinSpoofer/HWIDSpoofer/HWIDSpoofer/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace HWIDSpoofer
+{
+    class Program
+    {
+        
+        public static string textLogo =
+            @"" + Environment.NewLine +
+            @"$$$$$$$\            $$\                     $$\                  $$$$$$\                                 $$$$$$\                     " + Environment.NewLine +
+            @"$$  __$$\           \__|                    \__|                $$  __$$\                               $$  __$$\               " + Environment.NewLine +
+            @"$$ |  $$ | $$$$$$\  $$\  $$$$$$\   $$$$$$\  $$\ $$$$$$$\        $$ /  \__| $$$$$$\   $$$$$$\   $$$$$$\  $$ /  \__|$$$$$$\   $$$$$$\ " + Environment.NewLine +
+            @"$$ |  $$ |$$  __$$\ $$ |$$  __$$\ $$  __$$\ $$ |$$  __$$\       \$$$$$$\  $$  __$$\ $$  __$$\ $$  __$$\ $$$$\    $$  __$$\ $$  __$$\ " + Environment.NewLine +
+            @"$$ |  $$ |$$ |  \__|$$ |$$ /  $$ |$$ /  $$ |$$ |$$ |  $$ |       \____$$\ $$ /  $$ |$$ /  $$ |$$ /  $$ |$$  _|   $$$$$$$$ |$$ |  \__|" + Environment.NewLine +
+            @"$$ |  $$ |$$ |      $$ |$$ |  $$ |$$ |  $$ |$$ |$$ |  $$ |      $$\   $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |     $$   ____|$$ |      " + Environment.NewLine +
+            @"$$$$$$$  |$$ |      $$ |$$$$$$$  |$$$$$$$  |$$ |$$ |  $$ |      \$$$$$$  |$$$$$$$  |\$$$$$$  |\$$$$$$  |$$ |     \$$$$$$$\ $$ |      " + Environment.NewLine +
+            @"                        $$ |      $$ |                                    $$ |                                                       " + Environment.NewLine +
+            @"                        $$ |      $$ |                                    $$ |                                                       " + Environment.NewLine +
+            @"                        \__|      \__|                                    \__|                                                       " + Environment.NewLine; 
+
+
+        static void Main(string[] args) // 
+        {
+        begin:
+            Console.Title = "Drippin Spoofer ";
+            
+            Console.ResetColor();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine(textLogo);
+            Console.WriteLine("  ┌─────────────────────────────────────────┐");
+            Console.WriteLine("  │ [/hwid] Spoof  ur HWID                  │");
+            Console.WriteLine("  │ [/guid] Spoof ur Guid                   │");
+            Console.WriteLine("  │ [/pcName] Spoof ur Computer Name        │");
+            Console.WriteLine("  │ [/productId] Spoof ProductId            │");
+            Console.WriteLine("  └─────────────────────────────────────────┘");
+            Console.WriteLine("  ┌─────────────────────────────────────────┐");
+            Console.WriteLine("  │Discord ID: 476140629899083798           │");
+            Console.WriteLine("  └─────────────────────────────────────────┘");
+
+            string input = Console.ReadLine();
+            if(input == "/hwid") 
+            {
+                Console.Clear();
+                Console.WriteLine(textLogo);
+                if (Spoofer.HWID.Spoof())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(Spoofer.HWID.Log.ToString());
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(Spoofer.HWID.Log.ToString());
+                }
+                Console.ReadLine();
+                goto begin;
+            }
+            else if (input == "/guid")
+            {
+                Console.Clear();
+                Console.WriteLine(textLogo);
+                if (Spoofer.PCGuid.Spoof())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(Spoofer.PCGuid.Log.ToString());
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(Spoofer.PCGuid.Log.ToString());
+                }
+                Console.ReadLine();
+                goto begin;
+            }
+            else if (input == "/pcName")
+            {
+                Console.Clear();
+                Console.WriteLine(textLogo);
+                if (Spoofer.PCName.Spoof())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(Spoofer.PCName.Log.ToString());
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(Spoofer.PCName.Log.ToString());
+                }
+                Console.ReadLine();
+                goto begin;
+            }
+            else if (input == "/productId")
+            {
+                Console.Clear();
+                Console.WriteLine(textLogo);
+                if (Spoofer.ProductId.Spoof())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(Spoofer.ProductId.Log.ToString());
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(Spoofer.ProductId.Log.ToString());
+                }
+                Console.ReadLine();
+                goto begin;
+            }
+            else
+            {
+                goto begin;
+            }
+        }
+    }
+}
---
+++ DrippinSpoofer/HWIDSpoofer/HWIDSpoofer/Spoofer.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Text;
+using Microsoft.Win32;
+
+namespace HWIDSpoofer
+{
+    class Spoofer
+    {
+        public static class HWID
+        {
+            public static Regedit regeditOBJ = new Regedit(@"SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware Profiles\0001");
+            public static readonly string Key = "HwProfileGuid";
+
+            public static string GetValue()
+            {
+                return regeditOBJ.Read(Key);
+            }
+
+            public static bool SetValue(object value)
+            {
+                return regeditOBJ.Write(Key, value);
+            }
+
+            public static StringBuilder Log = new StringBuilder();
+            public static bool Spoof()
+            {
+                Log.Clear();
+                string oldValue = GetValue();
+                bool result =  SetValue("{" + Guid.NewGuid().ToString() + "}");
+                if (result)
+                {
+                    Log.Append("  [DRIPPIN] Old HWID " + oldValue + " New HWID " + GetValue());
+                }
+                else
+                {
+                    Log.AppendLine("  [DRIPPIN] Error! Pls run as admin!");
+                }
+                return result;
+            }
+        }
+
+        public static class PCGuid
+        {
+            public static Regedit regeditOBJ = new Regedit(@"SOFTWARE\Microsoft\Cryptography");
+            public static readonly string Key = "MachineGuid";
+
+            public static string GetValue()
+            {
+                return regeditOBJ.Read(Key);
+            }
+
+            public static bool SetValue(object value)
+            {
+                return regeditOBJ.Write(Key, value);
+            }
+
+            public static StringBuilder Log = new StringBuilder();
+            public static bool Spoof()
+            {
+                Log.Clear();
+                string oldValue = GetValue();
+                bool result = SetValue(Guid.NewGuid().ToString());
+                if (result)
+                {
+                    Log.Append("  [DRIPPIN] OLD GUID " + oldValue + " NEW GUID " + GetValue());
+                }
+                else
+                {
+                    Log.AppendLine("  [DRIPPIN] Error! Pls run as admin!");
+                }
+                return result;
+            }
+        }
+
+        public static class PCName
+        {
+            public static Regedit regeditOBJ = new Regedit(@"SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName");
+            public static readonly string Key = "ComputerName";
+
+            public static string GetValue()
+            {
+                return regeditOBJ.Read(Key);
+            }
+
+            public static bool SetValue(object value)
+            {
+                return regeditOBJ.Write(Key, value);
+            }
+
+            public static StringBuilder Log = new StringBuilder();
+            public static bool Spoof()
+            {
+                Log.Clear();
+                string oldValue = GetValue();
+                bool result = SetValue("DESKTOP-" + Utilities.GenerateString(15));
+                if (result)
+                {
+                    Log.Append("  [DRIPPIN] OLD Computer Name  " + oldValue + " NEW Computer Name " + GetValue());
+                }
+                else
+                {
+                    Log.AppendLine("  [DRIPPIN] Error! Pls Run As Admin!");
+                }
+                return result;
+            }
+        }
+
+        public static class ProductId
+        {
+            public static Regedit regeditOBJ = new Regedit(@"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion");
+            public static readonly string Key = "ProductID";
+
+            public static string GetValue()
+            {
+                return regeditOBJ.Read(Key);
+            }
+
+            public static bool SetValue(object value)
+            {
+                return regeditOBJ.Write(Key, value);
+            }
+
+            public static StringBuilder Log = new StringBuilder();
+            public static bool Spoof()
+            {
+                Log.Clear();
+                string oldValue = GetValue();
+                bool result = SetValue(Utilities.GenerateString(5) + "-" + Utilities.GenerateString(5) + "-" + Utilities.GenerateString(5) + "-" + Utilities.GenerateString(5));
+                if(result)
+                {
+                    Log.AppendLine("  [DRIPPIN] OLD Computer ProductID  " + oldValue + " NEW Computer ProductID " + GetValue());
+                }
+                else
+                {
+                    Log.AppendLine("  [DRIPPIN] Error! Pls Run As Admin!");
+                }
+                return result;
+            }
+        }
+
+        public class Regedit
+        {
+            private string regeditPath = string.Empty;
+            public Regedit(string regeditPath)
+            {
+                this.regeditPath = regeditPath;
+            }
+
+            public string Read(string keyName)
+            {
+                try
+                {
+                    using (RegistryKey key = Registry.LocalMachine.OpenSubKey(regeditPath))
+                    {
+                        if (key != null)
+                        {
+                            return key.GetValue(keyName).ToString();
+                        }
+                        else
+                        {
+
+                            return "ERR";
+                        }
+                    }
+                }
+                catch 
+                {
+
+                    return "ERR";
+                }
+            }
+
+            public bool Write(string keyName, object value)
+            {
+                try
+                {
+                    using (RegistryKey key = Registry.LocalMachine.OpenSubKey(regeditPath))
+                    {
+                        if (key != null)
+                        {
+                            key.SetValue(keyName, value);
+                            return true;
+                        }
+                        else
+                        {
+
+                            return false;
+                        }
+                    }
+                }
+                catch 
+                {
+
+                    return false;
+                }
+            }
+        }
+
+        public static class Utilities
+        {
+            private static Random rand = new Random();
+            public const string Alphabet = "ABCDEF0123456789";
+            private static Random random = new Random();
+            public const string Alphabet1 = "abcdef0123456789";
+
+            public static string GenerateString(int size)
+            {
+                char[] array = new char[size];
+                for (int i = 0; i < size; i++)
+                {
+                    array[i] = Alphabet[rand.Next(Alphabet.Length)];
+                }
+                return new string(array);
+            }
+        }
+    }
+}
